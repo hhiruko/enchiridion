c52e2c7d45cd0c432c693e74f0d4efac0c24ce76
hhiruko <adilet.toktybay@gmail.com>
2025-08-18 11:26:47 +0500

    feat: header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,35 @@
+import { Landmark, Scroll } from "lucide-preact";
 import { Page } from "./Page";
 import { ThemeButton } from "./ThemeButton";
 
 export function App() {
+    const handleCredits = () => {
+        const creditsContainer = document.querySelector('.credits-container');
+        if (!creditsContainer) {
+            return;
+        }
+
+        if(creditsContainer.style.display === 'block') {
+            creditsContainer.style.display = 'none';
+        } else {
+            creditsContainer.style.display = 'block';
+        }
+    };
+
     return (
         <>
-            <ThemeButton />
+            <header>
+                <h1><Landmark width={30} height={30} />The Enchiridion</h1>
+                <div id="header-right-container">
+                    <ThemeButton />
+                    <Scroll onClick={handleCredits} />
+                </div>
+            </header>
+            <div className="credits-container">
+                <h2>By Epictetus</h2>
+                <p>Written A.D. 135</p>
+                <p>Translated by Elizabeth Carter</p>
+            </div>
             <Page />
         </>
     );
